3f302c2bd3a51091341d7e526a712f0478aa4c8d
Forrest Jacobs <forrestjacobs@gmail.com>
2019-09-15 19:02:15 -0400

    Move tests to fixtures

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,4 +1,7 @@
-import { Parser } from "./parser";
+import { readdirSync, readFileSync } from "fs";
+import { safeLoadAll } from "js-yaml";
+import { Parser } from ".";
+import { Character } from "./character";
 
 const BASE_CHARACTER = {
   codepoints: {},
@@ -60,232 +63,33 @@ const BASE_CHARACTER = {
   nanori: []
 };
 
-describe("Parser", () => {
-  const dataHandler = jest.fn();
-  const errorHandler = jest.fn();
-  let parser: Parser;
+const FIXTURE_PATH = "./fixtures";
+const FIXTURES = readdirSync(FIXTURE_PATH).filter(filename =>
+  filename.endsWith(".yaml")
+);
 
-  beforeEach(() => {
-    jest.resetAllMocks();
-    parser = new Parser();
-    parser.on("data", dataHandler);
-    parser.on("error", errorHandler);
-  });
+describe.each(FIXTURES)("%s", filename => {
+  const yaml = readFileSync(`${FIXTURE_PATH}/${filename}`, "utf8");
+  const documents = safeLoadAll(yaml).map(document => [
+    document.name,
+    document
+  ]);
+  it.each(documents)("%s", (name, { xml, characters }) => {
+    const actualCharacters: Character[] = [];
+    const errorCallback = jest.fn();
 
-  test("parses a minimal kanjidic2 doc", () => {
-    parser.write("<kanjidic2>");
+    const parser = new Parser();
+    parser.on("data", character => actualCharacters.push(character));
+    parser.on("error", errorCallback);
 
-    parser.write("<character><literal>日</literal></character>");
-    expect(dataHandler).toBeCalledTimes(1);
-    expect(dataHandler).toHaveBeenNthCalledWith(1, {
-      ...BASE_CHARACTER,
-      literal: "日"
-    });
+    for (const part of xml) {
+      parser.write(part);
+    }
+    parser.end();
 
-    parser.end("</kanjidic2>");
-    expect(errorHandler).toHaveBeenCalledTimes(0);
-  });
-
-  test("ignores everything before <kanjidic2>", () => {
-    parser.write("Ye_u0MZ{]a%p9ratB]/' <ka");
-    parser.write("njidi");
-    parser.end("c2><character><literal>日</literal></character></kanjidic2>");
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "日"
-    });
-  });
-
-  test("parses multiple characters", () => {
-    parser.write("<kanjidic2>");
-
-    parser.write("<character><literal>一</literal></character>");
-    expect(dataHandler).toBeCalledTimes(1);
-    expect(dataHandler).toHaveBeenNthCalledWith(1, {
-      ...BASE_CHARACTER,
-      literal: "一"
-    });
-
-    parser.write("<character><literal>二</literal></character>");
-    expect(dataHandler).toBeCalledTimes(2);
-    expect(dataHandler).toHaveBeenNthCalledWith(2, {
-      ...BASE_CHARACTER,
-      literal: "二"
-    });
-
-    parser.end("</kanjidic2>");
-    expect(errorHandler).toHaveBeenCalledTimes(0);
-  });
-
-  test("parses a radical", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>亜</literal>
-          <radical>
-            <rad_value rad_type="classical">7</rad_value>
-          </radical>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "亜",
-      radicals: {
-        classical: 7
-      }
-    });
-  });
-
-  test("parses classical and nelson radicals", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>応</literal>
-          <radical>
-            <rad_value rad_type="classical">61</rad_value>
-            <rad_value rad_type="nelson_c">53</rad_value>
-          </radical>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "応",
-      radicals: {
-        classical: 61,
-        nelson_c: 53
-      }
-    });
-  });
-
-  test("parses meta", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>哀</literal>
-          <misc>
-            <grade>8</grade>
-            <stroke_count>9</stroke_count>
-            <freq>1715</freq>
-            <jlpt>1</jlpt>
-          </misc>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "哀",
-      grade: 8,
-      strokeCounts: [9],
-      freq: 1715,
-      jlpt: 1
-    });
-  });
-
-  test("parses multi-stroke counts", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>飴</literal>
-          <misc>
-            <stroke_count>13</stroke_count>
-            <stroke_count>14</stroke_count>
-          </misc>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "飴",
-      strokeCounts: [13, 14]
-    });
-  });
-
-  test("parses radical names", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>冂</literal>
-          <misc>
-            <stroke_count>2</stroke_count>
-            <rad_name>まきがまえ</rad_name>
-            <rad_name>えながまえ</rad_name>
-            <rad_name>どうがまえ</rad_name>
-            <rad_name>けいがまえ</rad_name>
-          </misc>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "冂",
-      strokeCounts: [2],
-      radNames: ["まきがまえ", "えながまえ", "どうがまえ", "けいがまえ"]
-    });
-  });
-
-  test("parses rm groups", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>决</literal>
-          <reading_meaning>
-            <rmgroup>
-              <reading r_type="ja_on">ケチ</reading>
-              <reading r_type="ja_on">ケツ</reading>
-              <reading r_type="ja_kun">き.める</reading>
-              <reading r_type="ja_kun">き.まる</reading>
-              <reading r_type="ja_kun">さ.く</reading>
-              <meaning>decide</meaning>
-              <meaning>determine</meaning>
-              <meaning>judge</meaning>
-            </rmgroup>
-          </reading_meaning>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "决",
-      readings: {
-        pinyin: [],
-        korean_r: [],
-        korean_h: [],
-        vietnam: [],
-        ja_on: ["ケチ", "ケツ"],
-        ja_kun: ["き.める", "き.まる", "さ.く"]
-      },
-      meanings: {
-        en: ["decide", "determine", "judge"]
-      }
-    });
-  });
-
-  test("parses nanori", () => {
-    parser.end(`
-      <kanjidic2>
-        <character>
-          <literal>咸</literal>
-          <reading_meaning>
-            <nanori>みな</nanori>
-            <nanori>しげ</nanori>
-          </reading_meaning>
-        </character>
-      </kanjidic2>
-    `);
-
-    expect(dataHandler).toHaveBeenCalledWith({
-      ...BASE_CHARACTER,
-      literal: "咸",
-      nanori: ["みな", "しげ"]
-    });
+    expect(actualCharacters).toMatchObject(
+      characters.map((c: Partial<Character>) => ({ ...BASE_CHARACTER, ...c }))
+    );
+    expect(errorCallback).toHaveBeenCalledTimes(0);
   });
 });
